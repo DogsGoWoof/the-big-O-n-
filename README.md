8d214f517e7f053898fed7bbd44dbe4d4a10a190
jason <jason_tan75@hotmail.com>
2024-06-07 16:58:32 -0400

    added in more styling and altered function methodology

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # the-big-O-n-
+
+
+Based on [Big O notation](https://en.wikipedia.org/wiki/Big_O_notation) where finding a certain value within a range as a function of time.
+Find _n_ using the help functions and make your best guess based on comparative probability. Fun for the whole family. Only fun with the whole family.
+
+### How to play
+Input area is for entering a `number` guess of what the `n-value` could be. The n-value is between the `min-value` (on the left) and the `max-value` (on the right). The player has __3 attempts__ before the game must be reset to play again.
+
+
+Help buttons below:
+    
+- __High^vlow__: Compares the number entered in the `input` field to to n-value and returns whether it is higher or lower. Changes a range value.
+- __EVEN+-oDd__: returns whether or not the n-value is even or odd.
+- __max-min-ing__: subtracts the min-value from the max-value (max - min) and compares the result to n-value. Changes a range value.
+- __div/iding e/nd__: divides the n-value by the min-value (n / min) and the max-value by the n-value (max / n) and compares results(ratios). Changes a range value (if there is a difference).
+
+Each help button can only be used once per game.
+
--- js/app.js
+++ js/app.js
@@ -7,91 +7,190 @@ const messageDisplayEl = document.getElementById("message");
 const highLowBtn = document.getElementById("high-low");
 const dividingEndsBtn = document.getElementById("dividends");
 const evenOddBtn = document.getElementById("even-odd");
+const maxMinIngBtn = document.getElementById("sub-tractor");
+const numberEls = document.getElementsByClassName("number");
+const buttonEls = document.getElementsByTagName("button");
+const pendingEl = document.getElementById("pending");
+const padEl = document.getElementById("pad");
+const divBtnEl = document.getElementById("button-container");
 
-
-const originalLimits = [];
-const currentLimits = [];
 let minRangeValue;
 let maxRangeValue;
 let nValue;
 let tries = 0;
-let lose = false;
 let win = false;
 
 const guess = () => {
-    const attempt = event.target.value;
-    tries++;
-    messageDisplayEl.textContent = verify(parseInt(attempt));
-    winLose();
+    if (event.key === "Enter" && tries < 3) {
+        const attempt = event.target.value;
+        guessEl.value = "";
+        tries++;
+        showTries(verify(parseInt(attempt)));
+        winLose();
+    }
 };
 
 const calcRange = (min, max) => {
-    const randomRangeValue = Math.floor(Math.random() * max + 1);
+    const randomRangeValue = Math.floor(Math.random() * (max - min) + 1) + min;
     nValue = randomRangeValue;
     minRangeValue = min;
     maxRangeValue = max;
-    console.log(nValue);
     return [min, nValue, max];
 };
 
 const verify = (number) => {
-    return number === nValue ? `On the dot` : `Off the mark ${tries}`;
+    if (number === nValue) {
+        win = true;
+        return `On the dot`;
+    } else if (number > nValue && (number - nValue) < 3) {
+        return `Apro-sO close`;
+    } else if (number < nValue && (nValue - number) < 3) {
+
+    } else {
+        return `Off the mark`;
+    }
 };
 
 const winLose = () => {
-    if (tries === 3) {
-        messageDisplayEl.textContent = `No more tries left. Try again?`;
+    if (tries === 3 && !win) {
+        messageDisplayEl.innerHTML = `No more tries left.<br> Try again?<br> Y/N?`;
+        resetPrompt()
+    } else if (win) {
+        padEl.classList.toggle("spin");
+        divBtnEl.classList.toggle("colorFlash");
+        messageDisplayEl.innerHTML = `You did it. You figured out a number.<br> Try again?<br> Y/N?`;
+        resetPrompt();
     }
 };
+
+const showTries = (message) => {
+    messageDisplayEl.innerHTML = message += `<br/> ${3 - tries} attempt(s) left`;
+}
 
 const updateRange = () => {
     minDisplayEl.textContent = minRangeValue;
     maxDisplayEl.textContent = maxRangeValue;
 };
 
-const highLow = (number)  => {
-    console.log(number);
-    console.log(nValue);
+const setRange = () => {
+    const min = 1;
+    const max = 3;
+    calcRange(min, max);
+};
+
+const randomNumbers = () => {
+    const randomNumber = Math.floor(Math.random() * (maxRangeValue - minRangeValue) + 1) + minRangeValue;
+    nDisplayEl.innerHTML = `<br>${randomNumber}`;
+};
+
+const highLow = () => {
+    const number = parseInt(guessEl.value);
     if (number <= nValue && number > minRangeValue) {
-        messageDisplayEl.textContent = `Within lower range`;
+        showTries(`Within lower range`);
         minRangeValue = number;
-        tries++;
+        highLowBtn.disabled = true;
     } else if (number >= nValue && number < maxRangeValue) {
-        messageDisplayEl.textContent = `Within higher range`;
+        showTries(`Within higher range`);
         maxRangeValue = number;
-        tries++;
+        highLowBtn.disabled = true;
     }
+    updateRange();
+    // highLowBtn.removeEventListener("click", highLow);
 };
 
 const evenOdd = () => {
-    console.log('test0');
-    tries++
-    nValue % 2 === 0 ? messageDisplayEl.textContent = `Even` : `Odd`;
-    evenOddBtn.removeEventListener("click", evenOdd);
-}
+    showTries(nValue % 2 === 0 ? `Even` : `Odd`);
+    // evenOddBtn.removeEventListener("click", evenOdd);
+    evenOddBtn.disabled = true;
+};
 
-const dividingEnds = (number) => {
-    const dividedValue = nValue / number;
-}
-
-guessEl.addEventListener("keypress", event => {
-    if (event.key === "Enter") {
-        guess();
-        guessEl.value = "";
+const maxMin = () => {
+    const maxAMinion = maxRangeValue - minRangeValue;
+    if (maxAMinion >= nValue) {
+        maxRangeValue = maxAMinion;
+        showTries(`min-ing max-value`);
+    } else {
+        minRangeValue = maxAMinion;
+        showTries(`max-ing min value`);
     }
-})
-
-highLowBtn.addEventListener("click", () => {
-    highLow(parseInt(guessEl.value));
     updateRange();
-});
+    // maxMinIngBtn.removeEventListener("click", maxMin);
+    maxMinIngBtn.disabled = true;
+};
 
-evenOddBtn.addEventListener("click", evenOdd);
+const dividingEnds = () => {
+    const dividedNValue = nValue / minRangeValue;
+    const dividedRangeValue = maxRangeValue / nValue;
+    if (dividedNValue > dividedRangeValue) {
+        minRangeValue *= Math.floor(dividedNValue / dividedRangeValue);
+        showTries(`min:n ratio is wider`);
+    } else if (dividedNValue === dividedRangeValue) {
+        showTries(`Equal ratios`);
+    } else {
+        maxRangeValue = Math.ceil(maxRangeValue *(dividedNValue / dividedRangeValue));
+        showTries(`n:max ratio is wider`);
+    }
+    updateRange();
+    // dividingEndsBtn.removeEventListener("click", dividingEnds); 
+    dividingEndsBtn.disabled = true;
+};
 
+const resetPrompt = () => {
+    disableButtons();
+    guessEl.type = "text";
+    guessEl.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            if (guessEl.value === "Y") {
+                reset();
+            } else if (guessEl.value === "N") {
+                for (const element in numberEls) {
+                    numberEls[element].textContent = ``;
+                };
+                messageDisplayEl.textContent = ``;
+                guessEl.value = ``;
+                clearInterval(interID);
+                guessEl.disabled = true;
+                nDisplayEl.innerHTML = `<div id="pending">.</div>`;
+            }
+        }
+    });
+}
+
+const reset = () => {
+    tries = 0;
+    guessEl.type = "number";
+    guessEl.value = "";
+    messageDisplayEl.textContent = "";
+    guessEl.addEventListener("keypress", guess);
+    highLowBtn.disabled = false;
+    dividingEndsBtn.disabled = false;
+    evenOddBtn.disabled = false;
+    maxMinIngBtn.disabled = false;
+    highLowBtn.addEventListener("click", highLow);
+    evenOddBtn.addEventListener("click", evenOdd);
+    dividingEndsBtn.addEventListener("click", dividingEnds);
+    maxMinIngBtn.addEventListener("click", maxMin);
+    interID = setInterval(randomNumbers, 100);
+    padEl.classList.remove("spin");
+    divBtnEl.classList.remove("colorFlash");
+};
+
+const disableButtons = () => {
+    for (const button in buttonEls) {
+        buttonEls[button].disabled = true;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// testing area
+///////////////////////////////////////////////////////////////////////////////////////////////
+let interID;
 const test = (a, b) => {
     const madeRange = calcRange(a, b);
     minDisplayEl.textContent = madeRange[0];
     maxDisplayEl.textContent = madeRange[2];
 }
 
-test(1, 100);
+reset();
+test(1, Math.ceil((Math.random() * 1000)));
+console.log(nValue);
